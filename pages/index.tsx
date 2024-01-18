84e98669fb94da52bcc6d2fc5efce651e78b9c87
YeeHan <cyh0011215@gmail.com>
2024-01-18 18:11:55 +0800

    feat: able to boot up env locally

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import React, { ReactNode } from "react";
+
+export default function Homepage(): JSX.Element {
+    return (
+        <>
+            <style jsx>{`
+                .eezee{
+                    color: "#2A64DB";
+                    font-size: "16px";
+                }
+            `}</style>
+
+            <p className="eezee">
+                Welcome to Eezee Take-home Assignment! Enjoy :)
+            </p>
+        </>
+    )
+}
